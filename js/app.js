7d0df573f97e56bf136f4e1f56b41c63b0c7ac18
yunyu950908 <yunyu950908@gmail.com>
2017-10-01 21:12:34 +0800

    tab

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@
     let slider = new Slider({
         el:document.querySelector("#slider"),
         slides:[
-            { link: "s1", image: "img/slider1.jpg" },
-            { link: "s2", image: "img/slider2.jpg" },
-            { link: "s3", image: "img/slider3.jpg" },
-            { link: "s4", image: "img/slider4.jpg" },
-            { link: "s5", image: "img/slider5.jpg" },
+            { link: "#", image: "img/slider1.jpg" },
+            { link: "#", image: "img/slider2.jpg" },
+            { link: "#", image: "img/slider3.jpg" },
+            { link: "#", image: "img/slider4.jpg" },
+            { link: "#", image: "img/slider5.jpg" },
         ]
     });
     
